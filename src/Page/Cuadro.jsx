b41faed1798cd456468648e0d9cfd87d56e96d94
NaiveCannonUT <riquicraft04@gmail.com>
2023-10-30 10:11:40 -0500

    mas cambios

--- src/Page/Cuadro.jsx
+++ src/Page/Cuadro.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js"
 
 
 function Cuadro() {
@@ -20,7 +21,7 @@ function Cuadro() {
                 </thead>
                 <tbody>
                     <tr>
-                        <td className="border px-4 py-2">1</td>
+                        <td className="border px-4 py-2">Mornning</td>
                         <td className="border px-4 py-2 bg-red-400"></td>
                         <td className="border px-4 py-2 bg-red-400"></td>
                         <td className="border px-4 py-2 bg-red-400"></td>
@@ -52,7 +53,7 @@ function Cuadro() {
                         <td className="border px-4 py-2 bg-red-400"></td>
                     </tr>
                     <tr>
-                        <td className="border px-4 py-2">2</td>
+                        <td className="border px-4 py-2">Noon</td>
                         <td className="border px-4 py-2 bg-amber-200"></td>
                         <td className="border px-4 py-2 bg-amber-200"></td>
                         <td className="border px-4 py-2 bg-amber-200"></td>
@@ -84,7 +85,7 @@ function Cuadro() {
                         <td className="border px-4 py-2 bg-amber-200"></td>
                     </tr>
                     <tr>
-                        <td className="border px-4 py-2">3</td>
+                        <td className="border px-4 py-2">Evenning</td>
                         <td className="border px-4 py-2 bg-teal-400"></td>
                         <td className="border px-4 py-2 bg-teal-400"></td>
                         <td className="border px-4 py-2 bg-teal-400"></td>
@@ -116,7 +117,7 @@ function Cuadro() {
                         <td className="border px-4 py-2 bg-teal-400"></td>
                     </tr>
                     <tr>
-                        <td className="border px-4 py-2">4</td>
+                        <td className="border px-4 py-2">Nigth</td>
                         <td className="border px-4 py-2 bg-blue-400"></td>
                         <td className="border px-4 py-2 bg-blue-400"></td>
                         <td className="border px-4 py-2 bg-blue-400"></td>
@@ -148,7 +149,7 @@ function Cuadro() {
                         <td className="border px-4 py-2 bg-blue-400"></td>
                     </tr>
                     <tr>
-                        <td className="border px-4 py-2">5</td>
+                        <td className="border px-4 py-2">Only when</td>
                         <td className="border px-4 py-2 bg-green-600"></td>
                         <td className="border px-4 py-2 bg-green-600"></td>
                         <td className="border px-4 py-2 bg-green-600"></td>
@@ -156,7 +157,7 @@ function Cuadro() {
                         <td className="border px-4 py-2 bg-green-600"></td>
                     </tr>
                     <tr>
-                        <td className="border px-4 py-2"></td>
+                        <td className="border px-4 py-2">i need it</td>
                         <td className="border px-4 py-2 bg-green-600"></td>
                         <td className="border px-4 py-2 bg-green-600"></td>
                         <td className="border px-4 py-2 bg-green-600"></td>
@@ -184,27 +185,39 @@ function Cuadro() {
                 </tbody>
             </table>
             <br></br>
-            <button data-bs-toggle="modal" data-bs-target="#exampleModal" type="button" className="p-2 bg-indigo-500 rounded-lg text-white">Añadir medicamento</button>
-
-            
-            <button type="button" class="btn btn-primary">
-                Launch demo modal
-            </button>
+            <button data-bs-toggle="modal" data-bs-target="#exampleModal" type="button" className="p-2 bg-indigo-500 rounded-lg text-white hover:bg-indigo-400">Añadir medicamento</button>
 
             
             <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                 <div class="modal-dialog">
                     <div class="modal-content">
                         <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                            <h5 class="modal-title" id="exampleModalLabel">Añadir medicamentos</h5>
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div class="modal-body">
-                            ...
+                            <form>
+                                <p className="p-2">Medicamento</p>
+                                <input className="border rounded-lg w-[95%] p-2"></input>
+
+                                <p className="p-2">Nombre clave</p>
+                                <input className="border rounded-lg w-[95%] p-2"></input>
+
+                                <p className="p-2">Descripcion (opcional)</p>
+                                <input className="border rounded-lg w-[95%] p-2"></input>
+
+                                <p>Seleccione su hora</p>
+                                <select className="flex p-2">
+                                <option className="border p-2 rounded-lg">Morning</option>
+                                <option className="border p-2 rounded-lg">Noon</option>
+                                <option className="border p-2 rounded-lg">Evening</option>
+                                <option className="border p-2 rounded-lg">Nigth</option>
+                                <option className="border p-2 rounded-lg">Only when I need it</option>
+                                </select>
+                            </form>
                         </div>
                         <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                            <button type="button" class="btn btn-primary">Save changes</button>
+                            <button className="bg-green-500 hover:bg-green-400 p-2 text-white rounded-lg">Agregar</button>
                         </div>
                     </div>
                 </div>
